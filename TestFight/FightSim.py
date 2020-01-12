d3131a02a57c29f4d222cce7ff7f8890180360e3
Taylor Isom <tisom3@gatech.edu>
2020-01-12 00:45:20 -0800

    adding py porj

--- TestFight/FightSim.py
+++ TestFight/FightSim.py
@@ -1,18 +1,31 @@
 import random
+from operator import attrgetter
+
+#TODO: implement movement later
 
 class Character:
-    name = "butt"
-    health = 100
     attacks = {}
 
 
 
-    def __init__(self, name, age):
+    def __init__(self, name, level=1, health=24, str=10, dex=10, con=10, int=10, wis=10, char=10, armor=10, speed=30):
         self.name = name
-        self.age = age
-        self.health = 99
+        self.level = level
+        self.health = health
+        self.currentHealth = health
+        self.strength = str
+        self.dexterity = dex
+        self.constitution = con
+        self.intelligence = int
+        self.wisdom = wis
+        self.charisma = char
+        self.armor = armor
+        self.speed = speed
+        self.initiative = 0
         self.initAttacks()
 
+    def __str__(self):
+        return ""+self.name + " (hp:" + str(self.currentHealth) + ")"
 
     def initAttacks(self):
         poke = Attack("poke", "4d4")
@@ -22,6 +35,16 @@ class Character:
         attack = random.choice( list(self.attacks) )
         return self.attacks.get(attack)
 
+    def attackTarget(self, target):
+        chosenAttack = self.attack()
+        damageDice = chosenAttack.damage
+        damage = rollDiceCommand(damageDice)
+        target.currentHealth -= damage
+        return damage
+
+    def rollInitiative(self):
+        self.initiative = rollDice(1, 20, calcAbilityMod( self.dexterity ) )
+        print(self.initiative, "init")
 
 
 class Attack:
@@ -29,6 +52,70 @@ class Attack:
     def __init__(self, name, damage):
         self.name = name
         self.damage = damage
+
+    def __str__(self):
+        return ""+self.name + ": " + self.damage
+
+
+class Fight:
+    initiativeOrder = []
+    charTurn = None
+    roundNumber = 1
+    isTeam1Alive = True
+    isTeam2Alive = True
+    results = None
+
+
+    def __init__(self, team1, team2):
+        self.team1 = team1
+        self.team2 = team2
+        self.setInitiativeOrder()
+
+    def rollInitiative(self, characters):
+        for character in characters:
+            character.rollInitiative()
+
+    def setInitiativeOrder(self):
+        characters = self.team1 + self.team2
+        self.rollInitiative(characters)
+        self.initiativeOrder = sorted(characters, key=attrgetter('initiative'))
+        self.initiativeOrder.reverse()
+
+    def updateTeamStatus(self):
+        self.isTeam1Alive = False
+        self.isTeam2Alive = False
+        for character in self.team1:
+            if character.currentHealth > 0:
+                self.isTeam1Alive = True
+                break
+        if not self.isTeam1Alive:
+            self.results = "Team 2 Wins"
+            return
+        for character in self.team2:
+            if character.currentHealth > 0:
+                self.isTeam2Alive = True
+                break
+        if not self.isTeam2Alive:
+            self.results = "Team 1 Wins"
+
+    def getFoe(self, character):
+        if character in self.team1:
+            return random.choice(self.team2)
+        else:
+            return random.choice(self.team1)
+
+    def fight(self):
+        while (self.isTeam1Alive and self.isTeam2Alive):
+            print("___________BEGINNING ROUND", self.roundNumber, "___________")
+            for character in self.initiativeOrder:
+                if (character.currentHealth > 0):
+                    foe = self.getFoe(character)
+                    damage = character.attackTarget(foe)
+                    print(character, "dealt", damage, "to", foe)
+            self.roundNumber += 1
+            print()
+            self.updateTeamStatus()
+        print(self.results)
 
 
 def rollDiceCommand(command):
@@ -48,7 +135,6 @@ def rollDiceCommand(command):
     # print(numTimesToRoll, dice, modifier)
     return rollDice(numTimesToRoll, dice, modifier)
 
-
 def rollDice(numTimesToRoll, dice, modifier = 0):
     sum = 0
     for i in range(numTimesToRoll):
@@ -62,16 +148,37 @@ def calcAbilityMod(score):
     mod = (score - 10) // 2
     return mod
 
+def printDict(dict):
+    for key, value in dict.items():
+        print(key, value)
+
+
 print()
 print( calcAbilityMod(7) )
 print()
 
 
-p = Character("peeee", 7)
-print(p.health, p.attack() )
-print(p)
+p = Character("fighter1")
+p2 = Character("fighter2")
+print(p, p.health, p.attack() )
+( printDict( p.attacks) )
 print()
 print( rollDiceCommand("4d4+1"))
 print()
 print( rollDice(4,4) )
+
+
+t1 = [p]
+t2 = [p2]
+fight = Fight(t1, t2)
+# p.currentHealth = 0
+fight.updateTeamStatus()
+print(fight.isTeam1Alive, fight.isTeam2Alive, fight.initiativeOrder)
+print()
+for char in fight.initiativeOrder:
+    print(char)
+
+print()
+fight.fight()
+print()
 
